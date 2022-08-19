ecda37867873375bee262892a89583934187b277
Sergey-98 <yatsenko1998@yandex.ru>
2022-08-19 15:28:54 +0400

    fix: fix console error

--- src/core/components/app/app.ts
+++ src/core/components/app/app.ts
@@ -1,11 +1,12 @@
-import { SprintGameApp } from '../sprintGame/sprintGameApp/sprintGameApp';
+import { SprintGameApp } from '../sprintGame/app/sprintGameApp';
 export default class App {
   public sprintGameApp: SprintGameApp; 
   constructor() {
     this.sprintGameApp = new SprintGameApp();
   }
-  start() {
-    this.sprintGameApp.start();
+  starting() {
+    this.sprintGameApp.starting();
+    console.log('durak')
   }
 }
 
