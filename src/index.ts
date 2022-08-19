5da2a9a839fefd6465cde9e4bfb23ad3bf6fcdb7
YanMSH <bromazot@ya.ru>
2022-08-19 13:52:17 +0500

    fix: delete temporary render logic

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import './global.css';
-import AuthPage from './pages/auth/auth-page';
 
 // import App from './core/components/app/app';
 
 // const app = new App();
-
-const page = new AuthPage();
-
-page.render();
