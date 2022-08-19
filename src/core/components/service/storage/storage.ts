49994d8ce1dcfcfebc63bd29a5ac27f9f485e969
YanMSH <bromazot@ya.ru>
2022-08-19 14:05:57 +0500

    fix: fixed double JSON.stringify on store.set
    Fixed logic from auth-page.ts

--- src/core/components/service/storage/storage.ts
+++ src/core/components/service/storage/storage.ts
@@ -1,8 +1,10 @@
+import { ResponseAuth } from '../../../types/loader-types';
+
 export default class Storage {
     public inStore(fieldName: string): boolean {
         return !!localStorage.getItem(fieldName);
     }
-    public set(fieldName: string, value: string): void {
+    public set(fieldName: string, value: string | ResponseAuth): void {
         localStorage.setItem(fieldName, JSON.stringify(value));
     }
     public get(fieldName: string): string | null {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import './global.css';
+import AuthPage from './pages/auth/auth-page';
 
 // import App from './core/components/app/app';
 
 // const app = new App();
+
+const page = new AuthPage();
+
+page.render();
