4185ffcfa3dbdb1860da187f988a837f93405bef
YanMSH <89582326+YanMSH@users.noreply.github.com>
2022-08-20 18:43:33 +0500

    feat: start implementing wordlist

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import './global.css';
+import WordListPage from './pages/wordlist/wordlist-page';
 
-import App from './core/components/app/app';
+// import App from './core/components/app/app';
 
-const app = new App();
+// const app = new App();
 
-app.starting();
+
+const page = new WordListPage();
+
+page.render();
+
