3a3f614858e7d34f2d1fe15750522c8c4fee7556
YanMSH <89582326+YanMSH@users.noreply.github.com>
2022-08-21 21:26:15 +0500

    feat: add links in header

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,18 @@ import './global.css';
 
 import WordListPage from './pages/wordlist/wordlist-page';
 import AuthPage from './pages/auth/auth-page';
+import { SprintGameApp } from './core/components/sprint-game/app/sprint-game-app';
 // import App from './core/components/app/app';
 
 // const app = new App();
 const auth = new AuthPage();
+const sprintGame = new SprintGameApp();
+
 const authLink = document.querySelector('.header__nav-auth') as HTMLElement;
+const sprintLink = document.querySelector('.header__nav-game-sprint') as HTMLElement;
+const wordListLink = document.querySelector('.header__nav-learn-wordlist') as HTMLElement;
+wordListLink.onclick = () => page.render();
+sprintLink.onclick = () => sprintGame.starting();
 authLink.onclick = () => { auth.render() }
 const page = new WordListPage();
 page.render();
