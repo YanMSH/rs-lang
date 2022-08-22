b4b17298fc173cc8f3f637090bba75c0d41a3bd5
Kristina Yatsenko <d.kristina27@yandex.rum>
2022-08-23 00:18:09 +0400

    feat: add timer to game and adaptive to layout of the game

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import './global.css';
-import WordListPage from './pages/wordlist/wordlist-page';
+// import WordListPage from './pages/wordlist/wordlist-page';
 
 import App from './core/components/app/app';
 
-// const app = new App();
+const app = new App();
 // app.renderAudioCall();
 
 // const page = new WordListPage();
 // page.render();
+app.starting();
