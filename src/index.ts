afe8becc17d4bf233b5a81f9e1009795e38e68e0
YanMSH <bromazot@ya.ru>
2022-08-31 18:48:08 +0500

    feat: add game buttons

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import TextbookPage from './pages/textbook/textbook-page';
 import AuthPage from './pages/auth/auth-page';
 import { SprintGameApp } from './pages/sprint-game/app/sprint-game-app';
 import AudioCallApp from './pages/audio-call/app/audio-call-app';
-import RegPage from './pages/registration/reg-page';
 // import App from './core/components/app/app';
 
 // const app = new App();
@@ -12,7 +11,7 @@ const textbook = new TextbookPage();
 const auth = new AuthPage();
 const sprintGame = new SprintGameApp();
 const audioCallGame = new AudioCallApp();
-const regPage = new RegPage();
+
 const authLink = document.querySelector('.header__nav-auth') as HTMLElement;
 const sprintLink = document.querySelector('.header__nav-game-sprint') as HTMLElement;
 const audioCallLink = document.querySelector('.header__nav-game-audiocall') as HTMLElement;
@@ -23,4 +22,4 @@ audioCallLink.onclick = () => audioCallGame.renderAudioCall();
 
 sprintLink.onclick = () => sprintGame.starting();
 authLink.onclick = () => auth.render();
-regPage.render();
+textbook.render();
