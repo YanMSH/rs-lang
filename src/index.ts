a1df7bede31f1692e257719369853aa57fbe1951
Sergey-98 <yatsenko1998@yandex.ru>
2022-09-04 15:22:35 +0400

    fix: corrected some comments

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import AuthPage from './pages/auth/auth-page';
 import { SprintGameApp } from './pages/sprint-game/app/sprint-game-app';
 import AudioCallApp from './pages/audio-call/app/audio-call-app';
 import MainPage from './pages/main/main-page';
-import StatisticController from './pages/statistic-page/statistic-controller/statistic-cotroller';
 // import App from './core/components/app/app';
 
 // const app = new App();
@@ -15,19 +14,21 @@ const textbook = new TextbookPage();
 const auth = new AuthPage();
 const sprintGame = new SprintGameApp();
 const audioCallGame = new AudioCallApp();
-const statistic = new StatisticController();
 
 const authLink = document.querySelector('.header__nav-auth') as HTMLElement;
 const sprintLink = document.querySelector('.header__nav-game-sprint') as HTMLElement;
 const audioCallLink = document.querySelector('.header__nav-game-audiocall') as HTMLElement;
 
 const textbookLink = document.querySelector('.header__nav-learn-textbook') as HTMLElement;
-const statisticLink = document.querySelector('.header__nav-stats') as HTMLElement;
 logoLink.onclick = () => mainPage.render();
 textbookLink.onclick = () => textbook.render();
 audioCallLink.onclick = () => audioCallGame.renderAudioCall();
 
 sprintLink.onclick = () => sprintGame.starting();
 authLink.onclick = () => auth.render();
-statisticLink.onclick = () => statistic.render();
 mainPage.render();
+
+import StatisticController from './pages/statistic-page/statistic-controller/statistic-cotroller';
+const statistic = new StatisticController();
+const statisticLink = document.querySelector('.header__nav-stats') as HTMLElement;
+statisticLink.onclick = () => statistic.render();
